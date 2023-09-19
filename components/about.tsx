db3060d9780fce50cd16936604a4285f761f2769
rafaelgomes <rafaelgomes2919@gmail.com>
2023-09-19 18:28:13 +0100

    About Me and CV changes

--- components/about.tsx
+++ components/about.tsx
@@ -22,14 +22,14 @@ export default function About() {
         After graduating with a degree in{" "}
         <span className="font-medium">Comunication and Multimedia</span>, I pursued my passion for programming by enrolling in a coding 
         bootcamp. During this program, I gained expertise in{" "}
-        <span className="font-medium">PHP and Adobe Magento</span>{" "}, and I particularly enjoy the problem-solving aspect of programming.
+        <span className="font-medium">PHP and Adobe Magento</span>, and I particularly enjoy the problem-solving aspect of programming.
          My core stack consists of{" "}
         <span className="font-medium">PHP, Adobe Magento, MySQL</span>, and I'm also proficient 
         in Next.js, Prisma, and MongoDB. I am constantly eager to explore and adopt new technologies.
       </p>
       <p>
         <span className="italic">Outside of coding</span>, I like to spend my
-        time with my friends, watching movies and go for walks. I enjoy spending time with friends, watching movies, and going for walks. 
+        time with my friends, watching movies and go for walks.
         I'm an avid learner and am currently delving 
         into the world of {" "}
         <span className="font-medium">musical production</span>.
